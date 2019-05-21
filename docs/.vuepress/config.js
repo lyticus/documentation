c108ccb0d850c54d18b4c2e72fb54a178ea2dd9e
Bjorn <krols.bj@gmail.com>
2019-05-21 11:06:14 +0200

    Update og tags

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,8 +12,44 @@ module.exports = {
     [
       "meta",
       {
+        property: "og:title",
+        content: "Lyticus Documentation"
+      }
+    ],
+    [
+      "meta",
+      {
+        property: "og:url",
+        content: "https://documentation.lyticus.com"
+      }
+    ],
+    [
+      "meta",
+      {
         property: "og:image",
-        content: "https://cdn.byteboomers.com/img/logo/png/lyticus--512x512.png"
+        content:
+          "https://cdn.byteboomers.com/img/logo/png/lyticus--1200x630.png"
+      }
+    ],
+    [
+      "meta",
+      {
+        property: "og:image:type",
+        content: "image/png"
+      }
+    ],
+    [
+      "meta",
+      {
+        property: "og:image:width",
+        content: "1200"
+      }
+    ],
+    [
+      "meta",
+      {
+        property: "og:image:height",
+        content: "630"
       }
     ]
   ],
