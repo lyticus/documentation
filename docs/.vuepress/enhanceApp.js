d8a11c486051ad7fcef659a942868c8d63bc4faf
Bjorn <krols.bj@gmail.com>
2019-06-05 17:57:41 +0200

    Integrated Sentry

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,10 @@
 import Lyticus from "lyticus";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
 
-export default ({ router }) => {
+export default ({ Vue, router }) => {
   if (typeof window !== "undefined") {
+    // Lyticus
     const lyticus = new Lyticus("-JX6EGyCq1HTtiOFYzOOt", {
       development: process.env.NODE_ENV === "development"
     });
@@ -9,5 +12,12 @@ export default ({ router }) => {
     router.afterEach(to => {
       lyticus.trackPage(to.fullPath);
     });
+    // Sentry
+    if (process.env.NODE_ENV !== "development") {
+      Sentry.init({
+        dsn: "https://a8d6df7825194e3187f92cfe7e88890a@sentry.io/1475730",
+        integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+      });
+    }
   }
 };
